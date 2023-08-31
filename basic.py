78c7d61308a3dedbadede6d9f3c76ffcaa29e08c
Urvashi2409 <urvashicode2410@gmail.com>
2023-08-31 18:38:29 +0530

    lists, dictionaries

---
+++ basic.py
@@ -0,0 +1,20 @@
+
+mylist = [1, 2, 3, 4, 45.6, 45.6, 45.6, True, 'tushar', 'a', 2j]
+# Q. to remove all the duplicates having 45.6 value 
+
+# print(mylist)
+# print(mylist[5])
+# print(mylist[6])
+
+# mylist[3] = 14 
+# print(mylist)
+
+# mylist[4] = 0
+# print(mylist)
+
+# del mylist[4]
+# mylist.remove(45.6)
+# mylist.pop(4)
+# print(mylist)
+
+mylist = ["John", "Bob", "Urvashi", "Hussain"]
